74f381fe9b0e99b57aa0c321c590bc7e6a6f3baf
Claudi Freitas <claudi@opportunityatwork.org>
2025-04-02 15:56:33 -0300

    Creates backend initial setup
    Running command:
    rails new . --api --database=postgresql --skip-ci --skip-thruster --skip-test --skip-kamal --skip-solid --skip-rubocop --skip-hotwire

---
+++ 01_build_local_project.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Existing containers - destroying..."
+docker stop $(docker container ls --format "{{.Names}}" | grep todo_backend)
+docker rm $(docker container ls --format "{{.Names}}" | grep todo_backend)
+docker compose down
+echo "Existing containers - done!"
+
+echo
+
+echo "Volumes - destroying..."
+docker volume rm $(docker volume ls -f dangling=true)
+echo "Volumes - done!"
+
+echo
+
+echo "Images - building..."
+docker rmi $(docker images -f dangling=true)
+docker rmi todo_backend
+docker compose build backend
+echo "Images - done!"
+
+# How to run:
+# bash 01_build_local_project.sh
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# my_vue_todo_list
+# My To-Do list
+
+For study purpose.
+
+## Software Stack
+
+The project is using the following stacks:
+
+* [PostgreSQL](https://www.postgresql.org/docs/15/index.html) - object-relational database
+* [Ruby on Rails](https://rubyonrails.org/2025/3/12/Rails-Version-8-0-2-has-been-released) - for backend API
+* Front end is implemented using [Vue.js](https://vuejs.org/guide/introduction.html).
